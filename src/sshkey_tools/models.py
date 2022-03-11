b3fd2eb38c79320317135863d6fb2b6933ca1e24
Lars Scheibling <lars@scheibling.se>
2022-03-11 12:05:00 +0100

    Added crypto functions

--- src/sshkey_tools/models.py
+++ src/sshkey_tools/models.py
@@ -1,22 +1,24 @@
 import os
 from dataclasses import dataclass
 from .crypto import Algorithm, RSA, DSA, ECDSA, ED25519
+from .exceptions import FileAccessException, PasswordNeededException
 
 @dataclass
 class KeyData:
-    key_data: str
+    key_data: str = None
+    password: str = None
     
-    def from_file(self, path: str) -> None:
+    @classmethod
+    def from_file(cls, path: str, password: str = None) -> None:
         try:
-            with open(path, 'r') as f:
-                self.key_data = f.read()
-        except FileNotFoundError:
-            raise FileNotFoundError(f"File {path} not found")
-        except PermissionError:
-            raise PermissionError(f"File {path} is not readable")
+            with open(path, 'rb') as f:
+                return cls(key_data=f.read(), password=password)
+        except (FileNotFoundError, PermissionError) as e:
+            raise FileAccessException(e)   
         
-    def from_string(self, private_key: str) -> None:
-        self.private_key = private_key
+    @classmethod
+    def from_string(cls, key_data: str) -> None:
+        return cls(key_data=key_data)
         
     def to_file(self, path: str, overwrite: bool = False) ->  None:
         if self.key_data is None:
@@ -25,53 +27,60 @@ class KeyData:
         if os.path.isfile(path) and not overwrite:
             raise FileExistsError(f"File {path} already exists, use overwrite=True to overwrite")
         
+        mode = 'wb' if isinstance(self.key_data, bytes) else 'w'
         try:
-            with open(path, 'w') as f:
+            with open(path, mode) as f:
                 f.write(self.key_data)
         except PermissionError:
             raise PermissionError(f"File {path} is not writable")
         
     def __str__(self):
-        return self.key_data
+        return self.key_data.decode('utf-8')
 
 @dataclass
 class PublicKey(KeyData):
     pass
 
-
 @dataclass
 class PrivateKey(KeyData):
-    alg: Algorithm
-    bits: int
-    password: str
-    name: str
+    alg: Algorithm = Algorithm
+    bits: int = 0
+    password: str = None
+    name: str = None
+    
+    def __post_init__(self):
+        if self.key_data is not None:
+            self.alg.load_private_key(self.key_data, self.password)
+        
+    @classmethod
+    def generate(cls, algorithm: Algorithm = None, bits: int = None) -> None:
+        init_class = cls(alg=algorithm(bits=bits), bits=bits)
+        init_class.alg.gen_private_key()
+        init_class.key_data = init_class.alg.get_private_key()
+        
+        return init_class
         
     def get_public_key(self) -> PublicKey:
         if self.key_data is None:
             raise ValueError("No private key has been loaded")
         
-        return "CHANGEME: PrivateKey"
-    
-    def generate(self, bits: int, type: str = 'ecdsa', generate_both: bool = False) -> None:
-        pass
-
+        return PublicKey(self.alg.get_public_key())
     
 @dataclass
 class CertificateAuthority:
     private_key: PrivateKey
     public_key: PublicKey
-    secret: str
-        
+    secret: str      
         
 @dataclass
 class Certificate(KeyData):
-    user_id: str
-    principals: list
-    serial: int
-    validity: str
-    ca: CertificateAuthority
-    public_key: PublicKey
-    certificate: str    
+    user_id: str = None
+    principals: list = None
+    serial: int = None
+    validity: str = None
+    ca: CertificateAuthority = None
+    public_key: PublicKey = None
+    certificate: str =  None
     
     def sign(self):
         pass
--- test.py
+++ test.py
@@ -1,1 +1,53 @@
 from src.sshkey_tools.models import PublicKey, PrivateKey, CertificateAuthority, Certificate
+from src.sshkey_tools.crypto import RSA, DSA, ECDSA, ED25519
+
+from dataclasses import dataclass
+from cryptography.hazmat.primitives import serialization as crypto_serialization
+from cryptography.hazmat.primitives import hashes as crypto_hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, dsa, ec, ed25519
+from cryptography.hazmat.backends import default_backend as crypto_default_backend
+
+
+with open('test_key', 'rb') as f:
+    key_data = f.read()
+
+
+key = crypto_serialization.load_pem_private_key(
+            key_data,
+            password='password',
+            backend=crypto_default_backend()
+        )
+
+print(key)
+
+
+
+# key = PrivateKey.generate(algorithm=ECDSA, bits=256)
+# key.to_file('keyfile_test')
+# print(str(key.get_public_key()))
+
+# key2 = PrivateKey.from_file('keyfile_test')
+# print(str(key2))
+# print(key2)
+# print(key2.alg)
+
+# key = PrivateKey.from_file('test_key', password='password'.encode('utf-8'))
+# print(str(key))
+
+
+# print(str(key))
+# print(str(key.get_public_key()))
+
+# with open('test.key', 'w') as f:
+#     f.write(str(key))
+    
+# with open('test.crt', 'w') as f:
+#     f.write(str(key.get_public_key()))
+
+
+# @classmethod for extra constructors
+
+# https://gist.github.com/thomdixon/bc3d664b6305adec9ecbc155b5ca3b6d
+# https://stackoverflow.com/questions/59243185/generating-elliptic-curve-private-key-in-python-with-the-cryptography-library
+# https://dev.to/aaronktberry/generating-encrypted-key-pairs-in-python-69b
+# https://cryptography.io/en/latest/hazmat/primitives/asymmetric/rsa/
