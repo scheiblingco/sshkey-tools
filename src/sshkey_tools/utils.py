9e47e52f8358d3491c948025fac7ea021554e56e
Lars Scheibling <lars@scheibling.se>
2022-07-01 15:17:18 +0000

    Started adding fieldsets Bugfix, concatenated code

--- src/sshkey_tools/utils.py
+++ src/sshkey_tools/utils.py
@@ -2,15 +2,19 @@
 Utilities for handling keys and certificates
 """
 import sys
+from types import NoneType
 from typing import Union, List, Dict
 from secrets import randbits
+from random import randint
+from uuid import uuid4
 from base64 import b64encode
 import hashlib as hl
 
 def ensure_string(
-    obj: Union[str, bytes, list, tuple, set, dict], 
-    encoding: str = 'utf-8'
-) -> Union[str, List[str], Dict[str, str]]:
+    obj: Union[str, bytes, list, tuple, set, dict, NoneType], 
+    encoding: str = 'utf-8',
+    required: bool = False
+) -> Union[str, List[str], Dict[str, str], NoneType]:
     """Ensure the provided value is or contains a string/strings
 
     Args:
@@ -20,8 +24,10 @@ def ensure_string(
     Returns:
         Union[str, List[str], Dict[str, str]]: Returns a string, list of strings or dictionary with strings
     """
-    if isinstance(obj, (str, bytes)):
-        return obj.decode(encoding) if isinstance(obj, bytes) else obj
+    if (obj is None and not required) or isinstance(obj, str):
+        return obj
+    elif isinstance(obj, bytes):
+        return obj.decode(encoding)
     elif isinstance(obj, (list, tuple, set)):
         return [ensure_string(o, encoding) for o in obj]
     elif isinstance(obj, dict):
@@ -30,9 +36,10 @@ def ensure_string(
         raise TypeError(f"Expected one of (str, bytes, list, tuple, dict, set), got {type(obj).__name__}.")
 
 def ensure_bytestring(
-    obj: Union[str, bytes, list, tuple, set, dict], 
-    encoding: str = 'utf-8'
-) -> Union[str, List[str], Dict[str, str]]:
+    obj: Union[str, bytes, list, tuple, set, dict, NoneType], 
+    encoding: str = 'utf-8',
+    required: bool = None
+) -> Union[str, List[str], Dict[str, str], NoneType]:
     """Ensure the provided value is or contains a bytestring/bytestrings
 
     Args:
@@ -42,8 +49,10 @@ def ensure_bytestring(
     Returns:
         Union[str, List[str], Dict[str, str]]: Returns a bytestring, list of bytestrings or dictionary with bytestrings
     """
-    if isinstance(obj, (str, bytes)):
-        return obj.encode(encoding) if isinstance(obj, str) else obj
+    if (obj is None and not required) or isinstance(obj, bytes):
+        return obj
+    elif isinstance(obj, str):
+        return obj.encode(encoding)
     elif isinstance(obj, (list, tuple, set)):
         return [ensure_bytestring(o, encoding) for o in obj]
     elif isinstance(obj, dict):
@@ -61,9 +70,9 @@ def concat_to_string(*strs, encoding: str = 'utf-8') -> str:
     Returns:
         str: Concatenated string
     """
-    return ''.join(ensure_string(st, encoding) for st in strs)
+    return ''.join(st if st is not None else "" for st in ensure_string(strs, encoding))
     
-def concat_to_bytes(*strs, encoding: str = 'utf-8') -> bytes:
+def concat_to_bytestring(*strs, encoding: str = 'utf-8') -> bytes:
     """Concatenates a list of strings or bytestrings to a single bytestring.
 
     Args:
@@ -73,7 +82,23 @@ def concat_to_bytes(*strs, encoding: str = 'utf-8') -> bytes:
     Returns:
         bytes: Concatenated bytestring
     """
-    return ''.join(ensure_bytestring(st, encoding) for st in strs)
+    return b"".join(st if st is not None else b"" for st in ensure_bytestring(strs, encoding=encoding))
+
+def random_keyid() -> str:
+    """Generates a random Key ID
+
+    Returns:
+        str: Random keyid
+    """
+    return uuid4()
+
+def random_serial() -> str:
+    """ Generates a random serial number 
+    
+    Returns:
+        int: Random serial
+    """
+    return randint(0, 2**64-1)
 
 def long_to_bytes(
     source_int: int, force_length: int = None, byteorder: str = "big"
